91d36faee9d505c76fb41e9f92fe0f46018756e6
Timur Badretdinov <destinerx@gmail.com>
2020-06-23 17:42:59 +0300

    Don't try to read .env file in validate script

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,10 +1,3 @@
-// Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'development';
-process.env.NODE_ENV = 'development';
-
-// Ensure environment variables are read.
-require('../config/env');
-
 const fs = require('fs');
 const ethers = require('ethers');
 
